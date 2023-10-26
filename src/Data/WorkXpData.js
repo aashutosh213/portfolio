fc7dcc6870c2c7aad0398cdae543b1b422a2b072
Aashutosh Tiwari <64861698+aashutosh213@users.noreply.github.com>
2023-10-26 23:45:51 +0530

    workxpData made

---
+++ src/Data/WorkXpData.js
@@ -0,0 +1,12 @@
+export const workXpData = [
+    {
+        title: "Katonic",
+        designation:"Frontend Engineering Intern",
+        duration: "Mar,2022 - May,2022",
+        workedOn:[
+            "Developed and maintained the primary company website, increasing monthly traffic by 20% through regular content updates and optimizations.",
+            "Utilized Docusaurus to create comprehensive documentation for company products, resulting in a 30% decrease in customer support inquiries.                    ",
+            "Implemented a robust CICD pipeline for the documentation site, reducing deployment time and ensuring timely updates for customers."
+        ]
+    }
+];
